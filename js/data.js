09d48d8f1b9f4c1fd1f03b6716467721ae60487b
Nathaly Alarcon <nathaly@Nathalys-MacBook-Pro.local>
2018-04-23 00:30:59 -0400

    Initial Version - Many things to solve...

---
+++ js/data.js
@@ -0,0 +1,255 @@
+// Result of https://bio.torre.co/api/bios/nathalyalarcont API
+torreBio = {
+    "person": {
+        "id": 2517,
+        "publicId": "nathalyalarcont",
+        "name": "Nathaly Alarcón",
+        "email": "enat87@gmail.com",
+        "showEmail": true,
+        "professionalHeadline": "Data Scientist - I code in my sleep -  I love Coffee  - Women in Data Science Ambassador #WiDS2018",
+        "location": "La Paz, Bolivia",
+        "summaryOfBio": "Masters degree on Data Science. Passionate for techniques of Machine learning and Deep learning. Developer and technical leader in BPM projects. Also freelancer. Entrepreneur. Data Science Evangelist in Communities of developers (GDG La Paz, GDG Lima, Women Techmakers) . Stanford Women in Data Science Ambassador.",
+        "picture": "https://starrgate.s3.amazonaws.com:443/users/72d6e106d155f37d8c253f79131f2bdad60bbe2c/profile_oJ8q7tB.jpg",
+        "theme": "indigo700",
+        "weight": 0,
+        "itemsVerified": 1,
+        "recommendations": 0,
+        "completion": 0.6667,
+        "verified": true,
+        "claimant": true,
+        "featureDiscovery": false,
+        "canary": false,
+        "media": [],
+        "links": [
+            {
+                "id": 2524,
+                "name": "github",
+                "address": "https://github.com/nathalyalarcont"
+            },
+            {
+                "id": 2525,
+                "name": "facebook",
+                "address": "https://www.facebook.com/nathalyAlarconT"
+            },
+            {
+                "id": 2526,
+                "name": "twitter",
+                "address": "https://twitter.com/NathalyAlarconT"
+            },
+            {
+                "id": 2527,
+                "name": "linkedin",
+                "address": "https://www.linkedin.com/in/nathalyalarcon"
+            }
+        ]
+    },
+    "stats": {
+        "jobs": 4,
+        "achievements": 2,
+        "strengths": 5
+    },
+    "achievements": [
+        {
+            "id": 15166,
+            "category": "achievements",
+            "name": "Local Winner of Startup Weekend Women Artificial Intelligence Edition",
+            "organizations": [],
+            "weight": 0,
+            "verifications": 0,
+            "recommendations": 0,
+            "media": []
+        },
+        {
+            "id": 15165,
+            "category": "achievements",
+            "name": "Google Scholarship to attend to TensorFlow Dev Summit.",
+            "organizations": [],
+            "weight": 0,
+            "verifications": 0,
+            "recommendations": 0,
+            "media": []
+        }
+    ],
+    "strengths": [
+        {
+            "id": 3412,
+            "name": "Management",
+            "weight": 0,
+            "recommendations": 0,
+            "media": [],
+            "stats": [
+                {
+                    "category": "jobs",
+                    "count": 1
+                }
+            ]
+        },
+        {
+            "id": 3411,
+            "name": "Data Science",
+            "weight": 0,
+            "recommendations": 0,
+            "media": [],
+            "stats": [
+                {
+                    "category": "jobs",
+                    "count": 1
+                }
+            ]
+        },
+        {
+            "id": 3410,
+            "name": "Security ",
+            "weight": 0,
+            "recommendations": 0,
+            "media": [],
+            "stats": [
+                {
+                    "category": "jobs",
+                    "count": 1
+                }
+            ]
+        },
+        {
+            "id": 3409,
+            "name": "Development",
+            "weight": 0,
+            "recommendations": 0,
+            "media": [],
+            "stats": [
+                {
+                    "category": "jobs",
+                    "count": 1
+                }
+            ]
+        },
+        {
+            "id": 3407,
+            "name": "Data science",
+            "additionalInfo": "Master in Data Science, Deep Learning Specialist. Machine Learning Evangelist. ",
+            "weight": 0,
+            "recommendations": 0,
+            "media": [
+                {
+                    "group": "1ad219d1-d850-4ada-be72-2307a8ed34ae",
+                    "mediaType": "link",
+                    "description": "",
+                    "mediaItems": [
+                        {
+                            "id": 2428,
+                            "address": "http://www.widsconference.org/nathaly-alarcoacuten-torrico.html"
+                        }
+                    ]
+                },
+                {
+                    "group": "6cb0b27a-ab3b-4d50-85d2-49ed3c159854",
+                    "mediaType": "media",
+                    "description": "",
+                    "mediaItems": [
+                        {
+                            "id": 2429,
+                            "address": "https://bio-media.s3-us-west-2.amazonaws.com/people/2517/6cb0b27a-ab3b-4d50-85d2-49ed3c159854/6a5a17bb-a020-4316-bc05-688ff37573b6.jpg",
+                            "metadata": "{\"width\":1200,\"height\":703,\"orientation\":-1,\"focalPointX\":0.4852941176470588,\"focalPointY\":0.27612752070956403}"
+                        }
+                    ]
+                }
+            ],
+            "stats": [
+                {
+                    "category": "jobs",
+                    "count": 2
+                },
+                {
+                    "category": "achievements",
+                    "count": 2
+                }
+            ]
+        }
+    ],
+    "jobs": [
+        {
+            "id": 15170,
+            "category": "jobs",
+            "role": "Technical Lead\n",
+            "organizations": [
+                {
+                    "id": 659,
+                    "name": "ProcessMaker"
+                }
+            ],
+            "fromMonth": "January",
+            "fromYear": "2011",
+            "toMonth": "October",
+            "toYear": "2013",
+            "additionalInfo": "Technical Lead. I was in charge of the development of BPM solutions, under the tool ProcessMaker, I was in charge of a team of developers. Implement solutions for companies such as JosaBank, Kaikaia (Tutator Foundation), Stanford. I started as a Junior Process Architect, rapidly increasing positions due to my performance, and quick solution to problems.\n",
+            "weight": 0,
+            "verifications": 1,
+            "recommendations": 0,
+            "media": []
+        },
+        {
+            "id": 15169,
+            "category": "jobs",
+            "role": "Systems Chief",
+            "organizations": [
+                {
+                    "id": 8418,
+                    "name": "Sistemas interconectados Financieros - Pagaltoke"
+                }
+            ],
+            "fromMonth": "September",
+            "fromYear": "2013",
+            "toMonth": "May",
+            "toYear": "2015",
+            "additionalInfo": "Responsible for the Implementation of the Multibank Transactional Platform of Branchless Bank Agents (www.pagaltoke.com). I have coordinated with banks (BBVA Continental, ScotiaBank) and suppliers. I’ve also develop the infrastructure and architecture of the network on its technical and security aspects. I was also in charge of automating the internal processes of the company. I’ve also build machine learning models to detect incomplete transactions.\n",
+            "weight": 0,
+            "verifications": 0,
+            "recommendations": 0,
+            "media": []
+        },
+        {
+            "id": 15168,
+            "category": "jobs",
+            "role": "Data Scientist",
+            "organizations": [
+                {
+                    "id": 8416,
+                    "name": "Mojix"
+                }
+            ],
+            "fromMonth": "November",
+            "fromYear": "2016",
+            "toMonth": "",
+            "toYear": "",
+            "additionalInfo": "Design and control of Internet platform of things and Big Data. Use of RFID sensors, distributed programming with Spark and use of cloud environment with containers. Analysis and optimization of data processing. Analytics Platform development.",
+            "weight": 0,
+            "verifications": 0,
+            "recommendations": 0,
+            "media": []
+        },
+        {
+            "id": 15167,
+            "category": "jobs",
+            "role": "Data Scientist",
+            "organizations": [
+                {
+                    "id": 8419,
+                    "name": "Villca Solutions"
+                }
+            ],
+            "fromMonth": "January",
+            "fromYear": "2017",
+            "toMonth": "January",
+            "toYear": "2017",
+            "additionalInfo": "",
+            "weight": 0,
+            "verifications": 0,
+            "recommendations": 0,
+            "media": []
+        }
+    ],
+    "projects": [],
+    "publications": [],
+    "education": [],
+    "interests": []
+}
